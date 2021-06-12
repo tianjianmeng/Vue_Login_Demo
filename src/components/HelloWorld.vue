e8c03b6f573dcc8599605def8e9bcebb46c61ccc
扭曲树黑色_______ <42976780+tianjianmeng@users.noreply.github.com>
2021-06-12 13:01:48 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <p>{{message}}</p>
+        <input v-model="username" placeholder="username"><br/>
+        <input v-model="password" placeholder="password"><br/>
+        <button v-on:click="login">login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+	data(){
+  			return{
+  				message:'please login',
+  				username: '',
+  				password: ''
+  			}
+  		}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
