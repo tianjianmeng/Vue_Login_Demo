c13578e1203cae63938d217b420e270297aef88d
扭曲树黑色_______ <42976780+tianjianmeng@users.noreply.github.com>
2021-06-16 21:49:14 +0800

    0.4

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,11 @@
 <template>
 	<div class="homepage">
-	<p>{{message}}</p>
-	  <div class="static"
+		<div v-bind:class="{ 'active': A }">
+			<p>{{message}}</p>
+		</div>
+		
+		<button @click="e()">B</button>
+	  <div>
 	     <div v-bind:style="{ color: 'green', fontSize: '160px' }">{{message2}}</div>
 	  </div>
 	</div>
@@ -12,11 +16,24 @@
 	  name: 'home',
 		data(){
 	  			return{
+					message1:'homepage11111111111111111',
 	  				message:'homepage',
 					message2:'This is homepage',
-	  			}
-	  	}
+					A: false ,
+					}
+	  	},
+	  	methods: {
+			e(){
+
+				this.A = true;
+
+			}
+		}
 	}
 </script>
 <style>
+	.active{
+		color: #00FF00;
+		font-size: 89px;
+	}
 </style>
